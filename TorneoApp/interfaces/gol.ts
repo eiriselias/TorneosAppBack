648eef4993832ce9bedf6bdbd779e49caa9cd083
Eiris Salazar <86639872+eiriselias@users.noreply.github.com>
2025-04-29 11:32:50 -0500

    Delete TorneoApp directory

--- TorneoApp/interfaces/gol.ts
+++
@@ -1,8 +0,0 @@
-import { IEquipo } from "./equipo";
-import { IJugador } from "./jugador";
-
-export interface IGol{
-    player: IJugador,
-    team: IEquipo,
-    autogol: boolean
-}
--- TorneoApp/interfaces/jugador.ts
+++
@@ -1,8 +0,0 @@
-import { IGol } from "./gol";
-
-export interface IJugador{
-    name: string,
-    number: number,
-    position: "portero"|"defensor"|"mediocampista"|"delantero",
-    goals: IGol[]
-}
